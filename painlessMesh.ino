2e38305ccad5ec35555c85a3ecac6960d4edc521
BlackEdder <edder@tkwsping.nl>
2016-11-08 21:36:02 +0000

    Simplify project layout

--- painlessMesh.ino
+++ painlessMesh.ino
@@ -1,24 +1,15 @@
 //************************************************************
-// this is a simple example that uses the easyMesh library
+// this is a simple example that uses the painlessMesh library
 //
-// 1. blinks led once for every node on the mesh
-// 2. blink cycle repeats every BLINK_PERIOD
-// 3. sends a silly message to every node on the mesh at a random time betweew 1 and 5 seconds
-// 4. prints anything it recieves to Serial.print
+// 1. sends a silly message to every node on the mesh at a random time betweew 1 and 5 seconds
+// 2. prints anything it recieves to Serial.print
 // 
 //
 //************************************************************
-#include <easyMesh.h>
-
-// some gpio pin that is connected to an LED... 
-// on my rig, this is 5, change to the right number of your LED.
-#define   LED             5       // GPIO number of connected LED
-
-#define   BLINK_PERIOD    1000000 // microseconds until cycle repeat
-#define   BLINK_DURATION  100000  // microseconds LED is on for
+#include "painlessMesh.h"
 
 #define   MESH_PREFIX     "whateverYouLike"
-#define   MESH_PASSWORD   "somethingSneeky"
+#define   MESH_PASSWORD   "somethingSneaky"
 #define   MESH_PORT       5555
 
 easyMesh  mesh;
@@ -28,8 +19,6 @@ uint32_t sendMessageTime = 0;
 void setup() {
   Serial.begin(115200);
     
-  pinMode( LED, OUTPUT );
-
 //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
   mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
 
@@ -43,18 +32,7 @@ void setup() {
 void loop() {
   mesh.update();
 
-  // run the blinky
-  bool  onFlag = false;
-  uint32_t cycleTime = mesh.getNodeTime() % BLINK_PERIOD;
-  for ( uint8_t i = 0; i < ( mesh.connectionCount() + 1); i++ ) {
-    uint32_t onTime = BLINK_DURATION * i * 2;    
-
-    if ( cycleTime > onTime && cycleTime < onTime + BLINK_DURATION )
-      onFlag = true;
-  }
-  digitalWrite( LED, onFlag );
-
-  // get next random time for send message
+ // get next random time for send message
   if ( sendMessageTime == 0 ) {
     sendMessageTime = mesh.getNodeTime() + random( 1000000, 5000000 );
   }
