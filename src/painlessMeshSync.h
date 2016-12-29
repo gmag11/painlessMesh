9c7333ac4be9f4eb8ec5656ec23c19c96ddc3489
German Martin <gmag11@gmail.com>
2016-12-29 07:03:43 +0100

    Reduce the number of time sync cicles to make debugging easier

--- src/painlessMeshSync.h
+++ src/painlessMeshSync.h
@@ -5,8 +5,8 @@
 
 #define SCAN_INTERVAL       10000
 #define SYNC_INTERVAL       7000
-#define TIME_SYNC_CYCLES    10 // should (must?) be an even number
 #define TIME_SYNC_INTERVAL  5000000
+#define TIME_SYNC_CYCLES    2 // should (must?) be an even number
 
 //uint32_t getNodeTime( void );
 
