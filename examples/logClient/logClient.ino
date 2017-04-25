39a8396dd97003806a9cbbc75d8042e47d0b75b5
BlackEdder <edder@tkwsping.nl>
2017-04-25 19:34:57 +0100

    Default to channel 1 for logClient/Server

--- examples/logClient/logClient.ino
+++ examples/logClient/logClient.ino
@@ -37,7 +37,7 @@ void setup() {
     
   mesh.setDebugMsgTypes( ERROR | STARTUP | CONNECTION );  // set before init() so that you can see startup messages
 
-  mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT, STA_AP, AUTH_WPA2_PSK, 6 );
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT, STA_AP, AUTH_WPA2_PSK, 1 );
   mesh.onReceive(&receivedCallback);
 
   // Add the task to the mesh scheduler
