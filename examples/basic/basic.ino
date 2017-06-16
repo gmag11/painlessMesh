462d5e05a8ad18c0bd9ae06fbfb0deb67d625b98
costyn <costynvd@gmail.com>
2017-06-16 10:18:28 -0400

    Rewrote basic.ino to use TaskScheduler Tasks

--- examples/basic/basic.ino
+++ examples/basic/basic.ino
@@ -3,7 +3,7 @@
 //
 // 1. sends a silly message to every node on the mesh at a random time betweew 1 and 5 seconds
 // 2. prints anything it recieves to Serial.print
-// 
+//
 //
 //************************************************************
 #include "painlessMesh.h"
@@ -12,39 +12,10 @@
 #define   MESH_PASSWORD   "somethingSneaky"
 #define   MESH_PORT       5555
 
+void sendMessage() ; // Prototype so PlatformIO doesn't complain
+
 painlessMesh  mesh;
-
-uint32_t sendMessageTime = 0;
-
-void setup() {
-  Serial.begin(115200);
-    
-//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
-  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
-
-  mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT );
-  mesh.onReceive(&receivedCallback);
-  mesh.onNewConnection(&newConnectionCallback);
-  mesh.onChangedConnections(&changedConnectionCallback);
-  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
-}
-
-void loop() {
-  mesh.update();
-
- // get next random time to send a message
-  if ( sendMessageTime == 0 ) {
-    sendMessageTime = mesh.getNodeTime() + random( 1000000, 5000000 );
-  }
-
-  // if the time is ripe, send everyone a message!
-  if ( sendMessageTime != 0 && sendMessageTime < mesh.getNodeTime() ){
-    String msg = "Hello from node ";
-    msg += mesh.getNodeId();
-    mesh.sendBroadcast( msg );
-    sendMessageTime = 0;
-  }
-}
+Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
 
 void receivedCallback( uint32_t from, String &msg ) {
   Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
@@ -60,4 +31,31 @@ void changedConnectionCallback() {
 
 void nodeTimeAdjustedCallback(int32_t offset) {
     Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
+
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT );
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+
+  mesh.scheduler.addTask( taskSendMessage );
+  taskSendMessage.enable() ;
+}
+
+void loop() {
+  mesh.update();
+}
+
+void sendMessage() {
+  String msg = "Hello from node ";
+  msg += mesh.getNodeId();
+  mesh.sendBroadcast( msg );
+  taskSendMessage.setInterval( random( TASK_SECOND * 1, TASK_SECOND * 5 ));
 }
