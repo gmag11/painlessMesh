2666e255567b5484700958a6b06c39db6c732b54
BlackEdder <edder@tkwsping.nl>
2018-04-20 21:58:03 +0100

    Named node example
    This new example is build on top of the current painlessMesh
    
    commit a3f7965c0c1a079ace8b19c9fb7647ff9b150e94
    Author: BlackEdder <edder@tkwsping.nl>
    Date:   Fri Apr 20 21:49:53 2018 +0100
    
        Fix errors and add to gitlab-ci
    
    commit 6c5a19afc529fe22b6cc9e1267bb587993201fa1
    Author: BlackEdder <edder@tkwsping.nl>
    Date:   Fri Apr 20 19:34:11 2018 +0100
    
        Implemented namedMesh on top of painlessMesh
    
    commit 32478d45f7aaaa8bc6a12fde6a0083fb9f66e56c
    Author: BlackEdder <edder@tkwsping.nl>
    Date:   Fri Apr 20 16:49:47 2018 +0100
    
        Initial example for named nodes

---
+++ examples/namedMesh/namedMesh.ino
@@ -0,0 +1,52 @@
+//************************************************************
+// this is a simple example that uses the painlessMesh library
+// 
+// This example shows how to build a mesh with named nodes
+//
+//************************************************************
+#include <namedMesh.h>
+
+#define   MESH_SSID       "whateverYouLike"
+#define   MESH_PASSWORD   "somethingSneaky"
+#define   MESH_PORT       5555
+
+Scheduler     userScheduler; // to control your personal task
+namedMesh  mesh;
+
+String nodeName = "logNode"; // Name needs to be unique
+
+Task taskSendMessage( TASK_SECOND*30, TASK_FOREVER, []() {
+    String msg = String("This is a message from: ") + nodeName + String(" for logNode");
+    String to = "logNode";
+    mesh.sendSingle(to, msg); 
+}); // start with a one second interval
+
+void setup() {
+  Serial.begin(115200);
+
+  mesh.setDebugMsgTypes(ERROR | DEBUG | CONNECTION);  // set before init() so that you can see startup messages
+
+  mesh.init(MESH_SSID, MESH_PASSWORD, &userScheduler, MESH_PORT);
+
+  mesh.setName(nodeName); // This needs to be an unique name! 
+
+  mesh.onReceive([](uint32_t from, String &msg) {
+    Serial.printf("Received message by id from: %u, %s\n", from, msg.c_str());
+  });
+
+  mesh.onReceive([](String &from, String &msg) {
+    Serial.printf("Received message by name from: %s, %s\n", from.c_str(), msg.c_str());
+  });
+
+  mesh.onChangedConnections([]() {
+    Serial.printf("Changed connection\n");
+  });
+
+  userScheduler.addTask(taskSendMessage);
+  taskSendMessage.enable();
+}
+
+void loop() {
+  userScheduler.execute(); // it will run mesh scheduler as well
+  mesh.update();
+}
