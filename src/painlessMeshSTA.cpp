fb2bc4ec6992bbd8c154724bc8c3875efd67b862
Brandon Harville <brandon.harville@gmail.com>
2018-12-15 10:02:07 -0500

    Compatibility of ESP8266 2.5
    Make checking of IP address unambigious

--- src/painlessMeshSTA.cpp
+++ src/painlessMeshSTA.cpp
@@ -49,7 +49,7 @@ void ICACHE_FLASH_ATTR painlessMesh::tcpConnect(void) {
 
     // TODO: We could pass this to tcpConnect instead of loading it here
 
-    if (_station_got_ip && WiFi.localIP() != 0) {
+    if (_station_got_ip && WiFi.localIP().isSet() != 0) {
         AsyncClient *pConn = new AsyncClient();
 
         pConn->onError([](void *, AsyncClient * client, int8_t err) {
