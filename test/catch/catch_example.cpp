6e2e69da0edfb44533c28f42ea2f18d30acab022
BlackEdder <edder@tkwsping.nl>
2019-04-02 14:41:46 +0100

    Add ArduinoJson as a git submodule
    This will make it easier to include ArduinoJson in any tests

--- test/catch/catch_example.cpp
+++ test/catch/catch_example.cpp
@@ -1,6 +1,8 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch2/catch.hpp"
 
+#include <ArduinoJson.h>
+
 unsigned int Factorial( unsigned int number ) {
     return number <= 1 ? number : Factorial(number-1)*number;
 }
