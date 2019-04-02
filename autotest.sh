83f681913fbe3a92a26b2004041f2f8b0a0addf1
BlackEdder <edder@tkwsping.nl>
2019-04-02 16:44:11 +0100

    Cleanup autotest script

--- autotest.sh
+++ autotest.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-export PLATFORMIO_BUILD_FLAGS="-D UNIT_TEST"
-#inotify-hookable -w src -c "platformio ci --board=nodemcuv2 --lib="." test/test_main.cpp"
-#inotify-hookable -w src -w test -c "platformio test"
-inotify-hookable -w src -w test -c "rm bin/catch_*; cmake . -G Ninja -DCMAKE_CXX_FLAGS=\"-Wall -Werror\"; ninja && run-parts --regex catch_ bin/"
+inotify-hookable -w src -w test -c "rm bin/catch_*; cmake . -DCMAKE_CXX_FLAGS=\"-Wall -Werror\"; make; run-parts --regex catch_ bin/"
