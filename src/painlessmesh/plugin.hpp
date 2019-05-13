b9ad780c5ac256a1e6c7a958a44e5b2f56e4d552
BlackEdder <edder@tkwsping.nl>
2019-05-13 12:30:21 +0100

    Support for adding anonymous tasks for the scheduler

--- src/painlessmesh/plugin.hpp
+++ src/painlessmesh/plugin.hpp
@@ -1,6 +1,9 @@
 #ifndef _PAINLESS_MESH_PLUGIN_HPP_
 #define _PAINLESS_MESH_PLUGIN_HPP_
 
+#define _TASK_STD_FUNCTION
+#include "TaskSchedulerDeclarations.h"
+
 #ifndef ARDUINOJSON_VERSION_MAJOR
 #include "ArduinoJson.h"
 typedef std::string TSTRING;
@@ -68,15 +71,21 @@ class BroadcastPackage : public protocol::PackageInterface {
  * - tasks?
  */
 template <typename T>
-class PluginHandler : public layout::Layout<T> {
+class PackageHandler : public layout::Layout<T> {
  public:
-  bool sendPackage(protocol::PackageInterface *pkg) {
+  ~PackageHandler() {
+    for (auto&& task : taskList) {
+      task->set(1, 1, NULL, NULL, NULL);
+      task->disable();
+    }
+  }
+
+  bool sendPackage(protocol::PackageInterface* pkg) {
     auto variant = protocol::Variant(pkg);
     // if single or neighbour with direction
     if (variant.routing() == router::SINGLE ||
         (variant.routing() == router::NEIGHBOUR && variant.dest() != 0)) {
       return router::send(variant, (*this));
-      ;
     }
 
     // if broadcast or neighbour without direction
@@ -89,7 +98,43 @@ class PluginHandler : public layout::Layout<T> {
     return false;
   }
 
+  void onPackage(int type, std::function<bool(protocol::Variant)> function) {
+    auto func = [&function](protocol::Variant var, std::shared_ptr<T>,
+                            uint32_t) { return function(var); };
+    callbackList.onPackage(type, func);
+  }
+
+  /**
+   * Add a task to the scheduler
+   *
+   * The task will be stored in a list and a reference to the task will be
+   * returned. When the task is disabled it is automatically removed from
+   * the lst and the task object is destruced unless the returned reference
+   * was stored by the caller of addTask.
+   */
+  std::shared_ptr<Task> addTask(Scheduler& scheduler, unsigned long aInterval,
+                                long aIterations,
+                                std::function<void()> aCallback) {
+    std::shared_ptr<Task> task =
+        std::make_shared<Task>(aInterval, aIterations, aCallback);
+    // std::shared_ptr<A> task = std::make_shared<A>();
+    // According to the standard this should always point to the same
+    // task, so we can use it to remove it
+    // taskList.insert(taskList.end(), task);
+    auto it = taskList.insert(taskList.end(), task);
+    // auto disableFunc = [it, this]() {};
+    // disableFunc();
+    task->set(aInterval, aIterations, aCallback, NULL, [it, this]() {
+      this->taskList.erase(it);
+    });
+    scheduler.addTask((*task));
+    task->enable();
+    return task;
+  }
+
+ protected:
   router::MeshCallbackList<T> callbackList;
+  std::list<std::shared_ptr<Task> > taskList = {};
 };
 
 }  // namespace plugin
--- test/catch/catch_plugin.cpp
+++ test/catch/catch_plugin.cpp
@@ -11,12 +11,6 @@ typedef std::string TSTRING;
 
 #include "fake_serial.h"
 
-#define TASK_FOREVER 0
-#define TASK_ONCE 0
-#define TASK_MILLISECOND 1
-#define TASK_SECOND 1000 * TASK_MILLISECOND
-#define TASK_MINUTE 60 * TASK_SECOND
-
 #include "painlessmesh/plugin.hpp"
 
 using namespace painlessmesh;
@@ -59,6 +53,11 @@ class BCustomPackage : public plugin::BroadcastPackage {
   size_t jsonObjectSize() { return JSON_OBJECT_SIZE(noJsonFields + 1); }
 };
 
+class MockConnection : public layout::Neighbour {
+ public:
+  bool addMessage(TSTRING msg) { return true; }
+};
+
 SCENARIO("We can send a custom package") {
   GIVEN("A package") {
     auto pkg = CustomPackage();
@@ -97,8 +96,59 @@ SCENARIO("We can send a custom package") {
       }
     }
   }
+
+  GIVEN("A package handler function") {
+    auto handler = plugin::PackageHandler<MockConnection>();
+    auto func = [](protocol::Variant variant) {
+      auto pkg = variant.to<CustomPackage>();
+      REQUIRE(pkg.routing == router::BROADCAST);
+      return false;
+    };
+    THEN("We can pass it to handler") { handler.onPackage(20, func); }
+  }
+
+  GIVEN("A package") {
+    auto handler = plugin::PackageHandler<MockConnection>();
+    auto pkg = CustomPackage();
+    THEN("We can call sendPackage") {
+      auto res = handler.sendPackage(&pkg);
+      REQUIRE(!res);
+    }
+  }
 }
 
+SCENARIO("We can add tasks to the taskscheduler") {
+  GIVEN("A couple of tasks added") {
+    Scheduler mScheduler;
+    auto handler = plugin::PackageHandler<MockConnection>();
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    auto task1 = handler.addTask(mScheduler, 0, 1, [&i]() { ++i; });
+    auto task2 = handler.addTask(mScheduler, 0, 3, [&j]() { ++j; });
+    auto task3 = handler.addTask(mScheduler, 0, 3, [&k]() { ++k; });
+    REQUIRE(task1.use_count() == 2);
+    REQUIRE(task2.use_count() == 2);
+
+    WHEN("Executing the tasks") {
+      THEN("They should be called and automatically removed") {
+        REQUIRE(i == 0);
+        REQUIRE(j == 0);
+        mScheduler.execute();
+        REQUIRE(i == 1);
+        mScheduler.execute();
+        REQUIRE(task1.use_count() == 1);
+        REQUIRE(i == 1);
+        REQUIRE(j == 2);
+        // Still kept in handler, because hasn't been executed 3 times yet
+        REQUIRE(task2.use_count() == 2);
+        REQUIRE(task3.use_count() == 2);
+        task3->disable();
+        REQUIRE(task3.use_count() == 1);
+      }
+    }
+  }
+}
 // We will test this by implementing the named mesh as a plugin
 SCENARIO("We want to implement a named mesh as a custom type") {
   // Note that every plugin will have a unique id (which is also the package
